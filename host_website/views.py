ca9141fce854fa05c74521eaf10b053ac04ee83c
pavitar122 <pavitar3335@gmail.com>
2023-12-20 22:26:53 +0530

    update

--- host_website/views.py
+++ host_website/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 import requests
+from data import data
 
 # Create your views here.
 def host(request):
-    api_url = "https://api.github.com/users/kamal63442/repos"
-    response = requests.get(api_url)
-    value = response.json()
+    value = data
     return render(request,'index.html', {'data':value})
